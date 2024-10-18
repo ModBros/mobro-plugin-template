8f63d7f1c49b1c047bc403d1b59cf0eb27c04553
Manfred Graf <manfred.graf91@gmail.com>
2024-10-19 01:31:00 +0200

    feat: added DI logger example

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
 using MoBro.Plugin.SDK;
 using MoBro.Plugin.SDK.Builders;
 using MoBro.Plugin.SDK.Enums;
@@ -8,18 +9,20 @@ namespace MoBro.Plugin.Template;
 
 public class Plugin : IMoBroPlugin
 {
+  private readonly ILogger _logger;
   private readonly IMoBroService _mobro;
   private readonly IMoBroScheduler _scheduler;
   private readonly IMoBroSettings _settings;
 
   private readonly Random _random;
 
-  public Plugin(IMoBroService mobro, IMoBroScheduler scheduler, IMoBroSettings settings)
+  public Plugin(IMoBroService mobro, IMoBroScheduler scheduler, IMoBroSettings settings, ILogger logger)
   {
     // inject all needed services via the constructor
     _mobro = mobro; // allows the plugin to interact with MoBro 
     _scheduler = scheduler; // scheduler for recurring tasks such as updating metrics
     _settings = settings; // used to retrieve the settings values
+    _logger = logger;
 
     _random = new Random(); // just to generate some metric values
   }
@@ -27,7 +30,10 @@ public class Plugin : IMoBroPlugin
   public void Init()
   {
     // Init() will be automatically called once upon initialization of the plugin
+    // Put any initialization code here that needs to run only once on plugin startup
     // There is also an asynchronous implementation available: InitAsync()
+
+    _logger.LogInformation("Initializing plugin");
 
     // create and register all metrics
     CreateAndRegisterMetrics();
@@ -40,6 +46,15 @@ public class Plugin : IMoBroPlugin
 
     // schedule a recurring task to update the dynamic metrics
     _scheduler.Interval(UpdateDynamicMetrics, TimeSpan.FromSeconds(updateFrequencySetting), TimeSpan.FromSeconds(5));
+  }
+
+  public void Shutdown()
+  {
+    // Shutdown() will be automatically called once to signal the plugin that it is about to be shut down
+    // Put any cleanup or other action (e.g. persisting data) here that needs to run before the plugin is shut down
+    // There is also an asynchronous implementation available: ShutdownAsync()
+
+    _logger.LogInformation("Shutting down plugin");
   }
 
   private void CreateAndRegisterMetrics()
